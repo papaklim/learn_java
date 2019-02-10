a212f001059cbd3a0844dc80096b3008373730f5
papaklim <papaklim@gmail.com>
2019-02-10 16:21:26 +0300

    add Task_5: convert binary to integer

---
+++ src/Task_5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Task_5 {
+    public static void main (String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число в двоичном формате: ");
+        String binary = in.nextLine();
+        int binary2int = Integer.parseInt(binary, 2);
+        System.out.printf("Результат перевода двоичного числа \"%s\" в десятичную систему: %d", binary, binary2int);
+
+    }
+}

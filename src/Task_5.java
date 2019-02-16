493c4ecafee233bb2a07640f7fadd122c7752819
papaklim <papaklim@gmail.com>
2019-02-16 22:44:28 +0300

    add Task_6: add multiplication table, rename files

--- src/Task_5.java
+++ src/Task_5.java
@@ -1,12 +1,20 @@
 import java.util.Scanner;
 
 public class Task_5 {
-    public static void main (String[] args){
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите число в двоичном формате: ");
-        String binary = in.nextLine();
-        int binary2int = Integer.parseInt(binary, 2);
-        System.out.printf("Результат перевода двоичного числа \"%s\" в десятичную систему: %d", binary, binary2int);
-
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int x = input.nextInt();
+        System.out.println("Таблица умножения x10 для числа " + x + ":");
+        System.out.printf("%d * 1 = " + x + "\n", x );
+        System.out.printf("%d * 2 = " + x * 2 + "\n", x);
+        System.out.printf("%d * 3 = " + x * 3 + "\n", x);
+        System.out.printf("%d * 4 = " + x * 4 + "\n", x);
+        System.out.printf("%d * 5 = " + x * 5 + "\n", x);
+        System.out.printf("%d * 6 = " + x * 6 + "\n", x);
+        System.out.printf("%d * 7 = " + x * 7 + "\n", x);
+        System.out.printf("%d * 8 = " + x * 8 + "\n", x);
+        System.out.printf("%d * 9 = " + x * 9 + "\n", x);
+        System.out.printf("%d * 10 = " + x * 10 + "\n", x);
     }
 }

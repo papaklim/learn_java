51753f5a8fcca6da6945bdc9821f9da9255e910d
papaklim <papaklim@gmail.com>
2019-02-20 23:48:10 +0300

    add Task_8: example with do-while

---
+++ src/Task_8.java
@@ -0,0 +1,12 @@
+public class Task_8 {
+    public static void main(String[] args) {
+        int counter = 1;
+        do {
+            System.out.print(counter + ", ");
+            counter++;
+        }
+        while(counter <= 50);
+
+        }
+
+    }
